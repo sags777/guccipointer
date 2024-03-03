8b0b044b7d252a4819f4d837bcfc9755ed440f3e
sags777 <sagar.pandeya1001@gmail.com>
2024-03-02 20:12:19 -0600

    refactored pointer container

--- pages/floatingpointer.tsx
+++ pages/floatingpointer.tsx
@@ -1,22 +1,9 @@
-import Pointer from '@/components/Pointer/Pointer'
-import { Box, styled } from '@mui/material'
-import React from 'react'
-
-const FloatingPointerContainer = styled(Box)(() => {
-  return {
-    left: 0,
-    right: 0,
-    marginLeft: 0,
-    marginRight: 0
-  }
-})
+import Pointer from "@/components/Pointer/Pointer";
+import { Box, styled } from "@mui/material";
+import React from "react";
 
 function floatingpointer() {
-  return (
-    <FloatingPointerContainer>
-        <Pointer />
-    </FloatingPointerContainer>
-  )
+  return <Pointer />;
 }
 
-export default floatingpointer
+export default floatingpointer;
